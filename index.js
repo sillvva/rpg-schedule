09f3d724f98e3bc599cb63a4da6120e2d2a03965
EC2 Default User <ec2-user@ip-172-31-6-37.us-east-2.compute.internal>
2019-01-09 04:08:09 +0000

    keep awake

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ client.on('ready', () => {
     server.listen(process.env.PORT || 5000);
     
     setInterval(function() {
-        http.get(process.env.HOST);
+        http.get(process.env.HOST.replace('https', 'http'));
     }, 300000); // every 5 minutes (300000)
 });
 
