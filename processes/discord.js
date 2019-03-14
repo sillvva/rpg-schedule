fd559d28831843af7cb9fb66624702312f81a4c5
Matt DeKok <matt.dekok@gmail.com>
2019-03-13 21:11:31 -0500

    Troubleshooting channel updates

--- processes/discord.js
+++ processes/discord.js
@@ -58,11 +58,7 @@ const discordProcesses = (readyCallback) => {
                 }
                 const member = message.channel.guild.members.array().find(m => m.user.id === message.author.id);
                 if (member) {
-                    if (true) { //member.hasPermission(discord.Permissions.MANAGE_CHANNELS)
-                        console.log({
-                            guild: message.channel.guild.id,
-                            channel: parts[0].replace(/\<\#|\>/g,'')
-                        });
+                    if (1 !== 1) { //member.hasPermission(discord.Permissions.MANAGE_CHANNELS)
                         GuildConfig.save({
                             guild: message.channel.guild.id,
                             channel: parts[0].replace(/\<\#|\>/g,'')
