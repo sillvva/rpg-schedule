ff89ad6428c16bfec8cc10727c56d0dcec6838e2
Matt DeKok <matt.dekok@gmail.com>
2019-03-26 19:22:11 -0500

    Game reminders

--- processes/discord.js
+++ processes/discord.js
@@ -185,8 +185,8 @@ const postReminders = async (client) => {
     let games = await Game.fetchAllBy({ when: 'datetime', reminder: { $in: ['15','30','60'] } });
     console.log(games.length);
     games.forEach(async game => {
+        console.log(game.timestamp - parseInt(game.reminder), new Date().getTime());
         if (game.timestamp - parseInt(game.reminder) > new Date().getTime()) return;
-        console.log(game.s, game.c);
         const guild = client.guilds.get(game.s);
         if (guild) {
             const channel = guild.channels.get(game.c);
