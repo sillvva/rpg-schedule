b46571bc2eced282bbf2f950fbaddb99c0d560ef
MGD3 <mdekok@agsense.net>
2019-05-15 12:21:15 -0500

    Login Test

--- routes/game.js
+++ routes/game.js
@@ -30,6 +30,7 @@ module.exports = (options) => {
                                 const tag = `${username}#${discriminator}`;
 
                                 const data = {
+                                    config: config,
                                     user: {
                                         ...response,
                                         ...{
