18afde30f7329b75270cdaa1304c5358d2942272
MGD3 <mdekok@agsense.net>
2019-08-21 15:59:18 -0500

    Reaction bug

--- src/processes/discord.ts
+++ src/processes/discord.ts
@@ -288,7 +288,7 @@ const discordProcesses = (readyCallback: () => {}) => {
                     game.save();
                 }
             }
-            
+            reaction.remove(user);
         }
     });
     
