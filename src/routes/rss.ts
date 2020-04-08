e071ebb10410f3a38561a8408b8b5275cff97bed
Matt DeKok <matt.dekok@gmail.com>
2020-04-08 12:15:09 -0500

    Bug Fix

--- src/routes/rss.ts
+++ src/routes/rss.ts
@@ -16,13 +16,13 @@ export default (options: any) => {
 
     let tag = "";
 
-    client.guilds.cache.forEach(guild => {
-      guild.members.cache.forEach(member => {
+    client.guilds.cache.forEach((guild) => {
+      guild.members.cache.forEach((member) => {
         if (member.id === uid) {
           tag = member.user.tag;
           guilds.push({
             id: guild.id,
-            name: guild.name
+            name: guild.name,
           });
         }
       });
@@ -33,14 +33,14 @@ export default (options: any) => {
         $in: guilds.reduce((i, g) => {
           i.push(g.id);
           return i;
-        }, [])
+        }, []),
       },
       timestamp: {
-        $gt: new Date().getTime()
+        $gt: new Date().getTime(),
       },
       dm: {
-        $ne: tag
-      }
+        $ne: tag,
+      },
     };
 
     const games: any[] = await Game.fetchAllBy(gameOptions);
@@ -59,25 +59,19 @@ export default (options: any) => {
       <link>https://rpg-schedule.herokuapp.com</link>
     </image>
     ${games
-      .map(game => {
+      .map((game) => {
         if (!game.discordGuild) return "";
 
         const date = Game.ISOGameDate(game);
         game.moment = {
           raw: `${game.date} ${game.time} UTC${game.timezone < 0 ? "-" : "+"}${Math.abs(game.timezone)}`,
           iso: date,
-          date: moment(date)
-            .utcOffset(parseInt(game.timezone))
-            .format(config.formats.dateLong),
-          calendar: moment(date)
-            .utcOffset(parseInt(game.timezone))
-            .calendar(),
-          from: moment(date)
-            .utcOffset(parseInt(game.timezone))
-            .fromNow()
+          date: moment(date).utcOffset(parseInt(game.timezone)).format(config.formats.dateLong),
+          calendar: moment(date).utcOffset(parseInt(game.timezone)).calendar(),
+          from: moment(date).utcOffset(parseInt(game.timezone)).fromNow(),
         };
 
-        game.slot = game.reserved.split(/\r?\n/).findIndex(t => t.trim().replace("@", "") === tag) + 1;
+        game.slot = game.reserved.split(/\r?\n/).findIndex((t) => t.trim().replace("@", "") === tag) + 1;
         game.signedup = game.slot > 0 && game.slot <= parseInt(game.players);
         game.waitlisted = game.slot > parseInt(game.players);
 
@@ -87,12 +81,9 @@ export default (options: any) => {
         <link>https://rpg-schedule.herokuapp.com/games/upcoming</link>
         <guid>https://rpg-schedule.herokuapp.com/games/view?g=${game._id.toString().slice(-12)}</guid>
         <description>
-          <![CDATA[<p>Discord Server: ${(guilds.find(g => g.id === game.s) || {}).name}</p><p>GM: ${game.dm}</p><p>Where: ${game.where.replace(/\&/g, "&amp;")}</p><p>When: ${
+          <![CDATA[<p>Discord Server: ${(guilds.find((g) => g.id === game.s) || {}).name}</p><p>GM: ${game.dm}</p><p>Where: ${game.where.replace(/\&/g, "&amp;")}</p><p>When: ${
           game.moment.date
-        }</p><p>${game.description
-          .trim()
-          .replace(/\&/g, "&amp;")
-          .replace(/\r?\n/g, "<br>")}</p>]]>
+        }</p><p>${game.description.trim().replace(/\&/g, "&amp;").replace(/\r?\n/g, "<br>")}</p>]]>
         </description>
       </item>`;
       })
@@ -109,13 +100,13 @@ export default (options: any) => {
 
     let tag = "";
 
-    client.guilds.cache.forEach(guild => {
-      guild.members.cache.forEach(member => {
+    client.guilds.cache.forEach((guild) => {
+      guild.members.cache.forEach((member) => {
         if (member.id === uid) {
           tag = member.user.tag;
           guilds.push({
             id: guild.id,
-            name: guild.name
+            name: guild.name,
           });
         }
       });
@@ -126,21 +117,22 @@ export default (options: any) => {
         $in: guilds.reduce((i, g) => {
           i.push(g.id);
           return i;
-        }, [])
+        }, []),
       },
       timestamp: {
-        $gt: new Date().getTime()
-      }
+        $gt: new Date().getTime(),
+      },
     };
 
     if (signedup) {
       gameOptions.$or = [{ dm: tag }, { reserved: { $regex: tag } }];
     }
 
-    const games: any[] = await Game.fetchAllBy(gameOptions);
+    const games = await Game.fetchAllBy(gameOptions);
 
     const { error, value } = ics.createEvents(
-      games.map(game => {
+      games.map((game) => {
+        game.updateReservedList();
         const d = new Date(game.timestamp);
         return {
           uid: `${game._id}@rpg-schedule.com`,
@@ -151,15 +143,16 @@ export default (options: any) => {
           categories: ["RPG Schedule", game.discordGuild.name],
           location: game.where,
           organizer: { name: game.dm, email: `${game.dm.replace(/[^a-z0-9_.]/gi, "")}@rpg-schedule.com` },
-          attendees: game.reserved
-            .split("\n")
-            .filter(r => r.trim().length > 0)
-            .map((r, i) => ({
-              name: r,
-              rsvp: game.players - i > 0,
-              email: `${r.replace(/[^a-z0-9_.]/gi, "")}@rpg-schedule.com`
-            })),
-          sequence: game.sequence
+          attendees: Array.isArray(game.reserved)
+            ? game.reserved
+                .filter((r) => r.tag.trim().length > 0)
+                .map((r, i) => ({
+                  name: r,
+                  rsvp: parseInt(game.players) - i > 0,
+                  email: `${r.id.replace(/[^a-z0-9_.]/gi, "")}@rpg-schedule.com`,
+                }))
+            : [],
+          sequence: game.sequence,
         };
       })
     );
