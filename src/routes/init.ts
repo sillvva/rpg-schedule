b24f157e0b245cb722372454379a1fc062c91531
Matt DeKok <matt.dekok@gmail.com>
2020-04-10 21:10:31 -0500

    Bug Fix

--- src/routes/init.ts
+++ src/routes/init.ts
@@ -74,20 +74,7 @@ export default (options: any) => {
           };
 
           // if (!req.session.status.lastRefreshed || req.session.status.lastRefreshed + 300 < moment().unix()) {
-            request(
-              {
-                url: "https://discordapp.com/api/v6/oauth2/token",
-                method: "POST",
-                headers: headers,
-                form: {
-                  client_id: process.env.CLIENT_ID,
-                  client_secret: process.env.CLIENT_SECRET,
-                  grant_type: "refresh_token",
-                  refresh_token: access.refresh_token,
-                  redirect_uri: process.env.HOST + config.urls.login.path,
-                  scope: "identify guilds"
-                }
-              },
+            request(requestData,
               function(error, response, body) {
                 if (error || response.statusCode !== 200) {
                   if (response.statusCode == 400) res.redirect(config.urls.login.path);
