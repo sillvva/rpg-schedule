ef2d5f13819e99da560e8561a34efdfea93e18d2
Matt DeKok <matt.dekok@gmail.com>
2020-04-22 13:11:42 -0500

    Move colors to appaux.ts

--- src/processes/discord.ts
+++ src/processes/discord.ts
@@ -1,6 +1,8 @@
 import discord, { TextChannel, Client, Message, UserResolvable, User } from "discord.js";
 import { DeleteWriteOpResultObject, FilterQuery, ObjectID } from "mongodb";
 import { Express } from "express";
+import fromPairs from "lodash/fromPairs";
+import toPairs from "lodash/toPairs";
 
 import { io } from "../processes/socket";
 import { GuildConfig } from "../models/guild-config";
@@ -148,6 +150,8 @@ const discordProcesses = (options: DiscordProcessesOptions, readyCallback: () =>
                       })
                     : [guild.channels.cache.array().find((c) => c instanceof TextChannel)];
 
+                const embedColor = toPairs(aux.colors).find(([name, color]) => color === guildConfig.embedColor);
+
                 let embed = new discord.MessageEmbed()
                   .setTitle(`RPG Schedule ${lang.config.CONFIGURATION}`)
                   .setColor(guildConfig.embedColor)
@@ -164,7 +168,7 @@ const discordProcesses = (options: DiscordProcessesOptions, readyCallback: () =>
                       }\n` +
                       `${lang.config.PRUNING}: \`${guildConfig.pruning ? "on" : "off"}\`\n` +
                       `${lang.config.EMBEDS}: \`${!(guildConfig.embeds === false) ? "on" : "off"}\`\n` +
-                      `${lang.config.EMBED_COLOR}: \`${guildConfig.embedColor}\`\n` +
+                      `${lang.config.EMBED_COLOR}: \`${embedColor ? embedColor[0] : guildConfig.embedColor}\`\n` +
                       `${lang.config.EMBED_USER_TAGS}: \`${guildConfig.embedMentions}\`\n` +
                       `${lang.config.EMOJI_JOIN}: \`${guildConfig.emojiAdd}\`\n` +
                       `${lang.config.EMOJI_LEAVE}: \`${guildConfig.emojiRemove}\`\n` +
@@ -172,9 +176,7 @@ const discordProcesses = (options: DiscordProcessesOptions, readyCallback: () =>
                       `${lang.config.RESCHEDULE_MODE}: \`${guildConfig.rescheduleMode}\`\n` +
                       `${lang.config.PASSWORD}: ${guildConfig.password ? `\`${guildConfig.password}\`` : "Disabled"}\n` +
                       `${lang.config.ROLE}: ${guildConfig.role ? `\`${guildConfig.role}\`` : "All Roles"}\n` +
-                      `${lang.config.MANAGER_ROLE}: ${
-                        guildConfig.managerRole ? `\`@${guildConfig.managerRole}\` and Server Admins` : "Server Admins"
-                      }\n` +
+                      `${lang.config.MANAGER_ROLE}: ${guildConfig.managerRole ? `\`@${guildConfig.managerRole}\` and Server Admins` : "Server Admins"}\n` +
                       `${lang.config.DROP_OUTS}: \`${guildConfig.dropOut ? `Enabled` : "Disabled"}\`\n` +
                       `${lang.config.LANGUAGE}: \`${guildConfig.lang}\`\n`
                   );
@@ -256,151 +258,8 @@ const discordProcesses = (options: DiscordProcessesOptions, readyCallback: () =>
             } else if (cmd === "embed-color") {
               if (canConfigure) {
                 let color = params.join("");
-                var colors = {
-                  aliceblue: "#f0f8ff",
-                  antiquewhite: "#faebd7",
-                  aqua: "#00ffff",
-                  aquamarine: "#7fffd4",
-                  azure: "#f0ffff",
-                  beige: "#f5f5dc",
-                  bisque: "#ffe4c4",
-                  black: "#000000",
-                  blanchedalmond: "#ffebcd",
-                  blue: "#0000ff",
-                  blueviolet: "#8a2be2",
-                  brown: "#a52a2a",
-                  burlywood: "#deb887",
-                  cadetblue: "#5f9ea0",
-                  chartreuse: "#7fff00",
-                  chocolate: "#d2691e",
-                  coral: "#ff7f50",
-                  cornflowerblue: "#6495ed",
-                  cornsilk: "#fff8dc",
-                  crimson: "#dc143c",
-                  cyan: "#00ffff",
-                  darkblue: "#00008b",
-                  darkcyan: "#008b8b",
-                  darkgoldenrod: "#b8860b",
-                  darkgray: "#a9a9a9",
-                  darkgreen: "#006400",
-                  darkkhaki: "#bdb76b",
-                  darkmagenta: "#8b008b",
-                  darkolivegreen: "#556b2f",
-                  darkorange: "#ff8c00",
-                  darkorchid: "#9932cc",
-                  darkred: "#8b0000",
-                  darksalmon: "#e9967a",
-                  darkseagreen: "#8fbc8f",
-                  darkslateblue: "#483d8b",
-                  darkslategray: "#2f4f4f",
-                  darkturquoise: "#00ced1",
-                  darkviolet: "#9400d3",
-                  deeppink: "#ff1493",
-                  deepskyblue: "#00bfff",
-                  dimgray: "#696969",
-                  dodgerblue: "#1e90ff",
-                  firebrick: "#b22222",
-                  floralwhite: "#fffaf0",
-                  forestgreen: "#228b22",
-                  fuchsia: "#ff00ff",
-                  gainsboro: "#dcdcdc",
-                  ghostwhite: "#f8f8ff",
-                  gold: "#ffd700",
-                  goldenrod: "#daa520",
-                  gray: "#808080",
-                  green: "#008000",
-                  greenyellow: "#adff2f",
-                  honeydew: "#f0fff0",
-                  hotpink: "#ff69b4",
-                  indianred: "#cd5c5c",
-                  indigo: "#4b0082",
-                  ivory: "#fffff0",
-                  khaki: "#f0e68c",
-                  lightyellow: "#ffffe0",
-                  lime: "#00ff00",
-                  limegreen: "#32cd32",
-                  linen: "#faf0e6",
-                  lavender: "#e6e6fa",
-                  lavenderblush: "#fff0f5",
-                  lawngreen: "#7cfc00",
-                  lemonchiffon: "#fffacd",
-                  lightblue: "#add8e6",
-                  lightcoral: "#f08080",
-                  lightcyan: "#e0ffff",
-                  lightgoldenrodyellow: "#fafad2",
-                  lightgrey: "#d3d3d3",
-                  lightgreen: "#90ee90",
-                  lightpink: "#ffb6c1",
-                  lightsalmon: "#ffa07a",
-                  lightseagreen: "#20b2aa",
-                  lightskyblue: "#87cefa",
-                  lightslategray: "#778899",
-                  lightsteelblue: "#b0c4de",
-                  magenta: "#ff00ff",
-                  maroon: "#800000",
-                  mediumaquamarine: "#66cdaa",
-                  mediumblue: "#0000cd",
-                  mediumorchid: "#ba55d3",
-                  mediumpurple: "#9370d8",
-                  mediumseagreen: "#3cb371",
-                  mediumslateblue: "#7b68ee",
-                  mediumspringgreen: "#00fa9a",
-                  mediumturquoise: "#48d1cc",
-                  mediumvioletred: "#c71585",
-                  midnightblue: "#191970",
-                  mintcream: "#f5fffa",
-                  mistyrose: "#ffe4e1",
-                  moccasin: "#ffe4b5",
-                  navajowhite: "#ffdead",
-                  navy: "#000080",
-                  oldlace: "#fdf5e6",
-                  olive: "#808000",
-                  olivedrab: "#6b8e23",
-                  orange: "#ffa500",
-                  orangered: "#ff4500",
-                  orchid: "#da70d6",
-                  palegoldenrod: "#eee8aa",
-                  palegreen: "#98fb98",
-                  paleturquoise: "#afeeee",
-                  palevioletred: "#d87093",
-                  papayawhip: "#ffefd5",
-                  peachpuff: "#ffdab9",
-                  peru: "#cd853f",
-                  pink: "#ffc0cb",
-                  plum: "#dda0dd",
-                  powderblue: "#b0e0e6",
-                  purple: "#800080",
-                  rebeccapurple: "#663399",
-                  red: "#ff0000",
-                  rosybrown: "#bc8f8f",
-                  royalblue: "#4169e1",
-                  tan: "#d2b48c",
-                  teal: "#008080",
-                  thistle: "#d8bfd8",
-                  tomato: "#ff6347",
-                  turquoise: "#40e0d0",
-                  saddlebrown: "#8b4513",
-                  salmon: "#fa8072",
-                  sandybrown: "#f4a460",
-                  seagreen: "#2e8b57",
-                  seashell: "#fff5ee",
-                  sienna: "#a0522d",
-                  silver: "#c0c0c0",
-                  skyblue: "#87ceeb",
-                  slateblue: "#6a5acd",
-                  slategray: "#708090",
-                  snow: "#fffafa",
-                  springgreen: "#00ff7f",
-                  steelblue: "#4682b4",
-                  violet: "#ee82ee",
-                  wheat: "#f5deb3",
-                  white: "#ffffff",
-                  whitesmoke: "#f5f5f5",
-                  yellow: "#ffff00",
-                  yellowgreen: "#9acd32",
-                };
-                if (colors[color]) {
-                  color = colors[color];
+                if (aux.colors[color]) {
+                  color = aux.colors[color];
                 } else if (!color.match(/[0-9a-f]{6}/i)) {
                   (<TextChannel>message.channel).send(lang.config.desc.EMBED_COLOR_ERROR);
                   return;
