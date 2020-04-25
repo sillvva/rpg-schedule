a57029ce6f61af37981f315ce95f80a0b5c3ece5
Matt DeKok <matt.dekok@gmail.com>
2020-04-25 11:43:11 -0500

    Better URL Regex

--- src/models/game.ts
+++ src/models/game.ts
@@ -322,7 +322,7 @@ export class Game implements GameModel {
       game.gameImage = "";
     }
 
-    const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$\%&'\(\)\*\+,;=.]+$/gi;
+    const urlRegex = /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)$/gi;
 
     let embed: MessageEmbed;
     if (guildConfig.embeds === false) {
