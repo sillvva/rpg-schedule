9bfbef663cb7ae5fe1996957395d704a645cf0c7
Matt DeKok <matt.dekok@gmail.com>
2020-04-27 13:56:07 -0500

    Add recur rule to Google Calendar links

--- src/appaux.ts
+++ src/appaux.ts
@@ -1,6 +1,8 @@
 import _ from "lodash";
-import moment from "moment";
+import moment, { weekdays } from "moment";
+import "moment-recur-ts";
 import axios from "axios";
+import { GameModel } from "./models/game";
 
 interface Path {
   path: string;
@@ -18,32 +20,32 @@ const patreonPledges = async () => {
     const url = `https://www.patreon.com/api/oauth2/api/campaigns/${campaignId}/pledges`;
     const response = await axios.get(url, {
       headers: {
-        authorization: `Bearer ${accessToken}`
-      }
+        authorization: `Bearer ${accessToken}`,
+      },
     });
     const data = response.data;
-    const rewards = data.included.filter(i => i.type === "reward" && i.id > 0);
-    const pledges = data.data.filter(i => i.type === "pledge" && i.id > 0);
-    const users = data.included.filter(i => i.type === "user" && i.id > 0);
+    const rewards = data.included.filter((i) => i.type === "reward" && i.id > 0);
+    const pledges = data.data.filter((i) => i.type === "pledge" && i.id > 0);
+    const users = data.included.filter((i) => i.type === "user" && i.id > 0);
     const result = [];
-    pledges.forEach(pledge => {
+    pledges.forEach((pledge) => {
       const rewardId = pledge.relationships.reward.data.id;
-      const reward = rewards.find(r => r.id === rewardId);
+      const reward = rewards.find((r) => r.id === rewardId);
       const patronId = pledge.relationships.patron.data.id;
-      const user = users.find(u => u.id === patronId);
+      const user = users.find((u) => u.id === patronId);
       result.push({
         patron: user,
-        reward: reward
+        reward: reward,
       });
     });
     return {
       status: "success",
-      data: result
+      data: result,
     };
   } catch (err) {
     return {
       status: "error",
-      error: err
+      error: err,
     };
   }
 };
@@ -87,7 +89,7 @@ const objectChanges = (before: Object | Array<any>, after: Object | Array<any>) 
       result[key] = value instanceof Object && before[key] instanceof Object ? objectChanges(value, before[key]) : value;
       if (Array.isArray(before[key])) {
         let arr = [];
-        for(let i in after[key]) {
+        for (let i in after[key]) {
           arr.push(after[key][i]);
         }
         result[key] = arr;
@@ -122,37 +124,69 @@ interface Event {
   description?: string;
 }
 
-const parseEventTimes = (date: string, time: string, timezone: number, event?: Event) => {
-  const isoutc = `${new Date(`${date} ${time} UTC${timezone < 0 ? "-" : "+"}${Math.abs(timezone)}`)
+const parseEventTimes = (event: GameModel) => {
+  const raw = `${event.date} ${event.time} UTC${event.timezone < 0 ? "-" : "+"}${Math.abs(event.timezone)}`;
+  const isoutcStart = `${new Date(raw)
     .toISOString()
     .replace(/[^0-9T]/gi, "")
     .slice(0, 13)}00Z`;
-  const rawDate = `${date} ${time} UTC${timezone < 0 ? "-" : "+"}${parseTimeZoneISO(timezone)}`;
+  const endTime = new Date(raw);
+  endTime.setHours(endTime.getHours() + parseFloat(event.runtime));
+  const isoutcEnd = `${endTime
+    .toISOString()
+    .replace(/[^0-9T]/gi, "")
+    .slice(0, 13)}00Z`;
+  const rawDate = `${event.date} ${event.time} UTC${event.timezone < 0 ? "-" : "+"}${parseTimeZoneISO(event.timezone)}`;
   const d = new Date(rawDate)
     .toISOString()
     .replace(/[^0-9T]/gi, "")
     .slice(0, 13);
 
   const convertExtras = [];
-  if (event && event.name) convertExtras.push(`&l=${escape(event.name)}`);
+  if (event.adventure) convertExtras.push(`&l=${escape(event.adventure)}`);
 
   const convert2Extras = [];
-  if (event && event.name) convert2Extras.push(`&msg=${escape(event.name)}`);
+  if (event.adventure) convert2Extras.push(`&msg=${escape(event.adventure)}`);
 
   const googleCalExtras = [];
-  if (event && event.name) googleCalExtras.push(`&text=${escape(event.name)}`);
-  if (event && event.location) googleCalExtras.push(`&location=${escape(`${event.location}`)}`);
-  if (event && event.description) googleCalExtras.push(``);
+
+  const days = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
+  const weekdays = event.weekdays.map((w, i) => w && days[i]).filter((w) => w);
+  if (weekdays.length === 0) weekdays.push(days[moment(event.date).weekday()]);
+
+  if (event.frequency === 1) {
+    googleCalExtras.push(`&recur=RRULE:FREQ=DAILY`);
+  }
+  if (event.frequency === 2) {
+    googleCalExtras.push(`&recur=RRULE:FREQ=WEEKLY;BYDAY=${weekdays.join(",")}`);
+  }
+  if (event.frequency === 3) {
+    googleCalExtras.push(`&recur=RRULE:FREQ=WEEKLY;INTERVAL=${event.xWeeks};BYDAY=${weekdays.join(",")}`);
+  }
+  if (event.frequency === 4) {
+    if (event.monthlyType === "date") {
+      googleCalExtras.push(`&recur=RRULE:FREQ=MONTHLY`);
+    } else if (event.monthlyType === "weekday") {
+      googleCalExtras.push(`&recur=RRULE:FREQ=MONTHLY;BYDAY=${moment(event.date).monthWeekByDay() + 1}${days[new Date(raw).getDay()]}`);
+    }
+  }
+
+  if (event.adventure) googleCalExtras.push(`&text=${escape(event.adventure)}`);
+  if (event.where) googleCalExtras.push(`&location=${escape(`${event.where}`)}`);
+  if (event.description) googleCalExtras.push(`&details=${escape(event.description)}`);
 
   return {
+    raw: raw,
     rawDate: rawDate,
-    isoutc: isoutc,
+    isoutc: isoutcStart,
+    isoutcStart: isoutcStart,
+    isoutcEnd: isoutcEnd,
     convert: {
       timee: `https://timee.io/${d}?${convertExtras.join("")}`,
-      timeAndDate: `https://www.timeanddate.com/worldclock/converter.html?iso=${d}&p1=1440`
+      timeAndDate: `https://www.timeanddate.com/worldclock/converter.html?iso=${d}&p1=1440`,
     },
     countdown: `https://www.timeanddate.com/countdown/generic?iso=${d}&p0=1440${convert2Extras.join("")}`,
-    googleCal: `http://www.google.com/calendar/render?action=TEMPLATE&dates=${isoutc}/${isoutc}&trp=false${googleCalExtras.join("")}`
+    googleCal: `http://www.google.com/calendar/render?action=TEMPLATE&dates=${isoutcStart}/${isoutcEnd}&trp=true${googleCalExtras.join("")}`,
   };
 };
 
@@ -169,7 +203,6 @@ const isEmoji = (emoji: string) => {
     /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g
   );
 };
-
 
 var colors = {
   aliceblue: "#f0f8ff",
@@ -327,5 +360,5 @@ export default {
   isEmoji: isEmoji,
   log: log,
   patreonPledges: patreonPledges,
-  colors: colors
+  colors: colors,
 };
