c645a8f5637fa36e0c05901a09512f0f9eb84429
Matt DeKok <matt.dekok@gmail.com>
2020-04-27 17:11:17 -0500

    Remove unused import

--- src/appaux.ts
+++ src/appaux.ts
@@ -1,5 +1,5 @@
 import _ from "lodash";
-import moment, { weekdays } from "moment";
+import moment from "moment";
 import "moment-recur-ts";
 import axios from "axios";
 import { GameModel } from "./models/game";
