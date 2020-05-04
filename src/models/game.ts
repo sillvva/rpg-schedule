73f2f886a36eeb04a0331db0f2dc4f6696e725c1
Matt DeKok <matt.dekok@gmail.com>
2020-05-03 20:08:40 -0500

    Bug Fix

--- src/models/game.ts
+++ src/models/game.ts
@@ -684,7 +684,8 @@ export class Game implements GameModel {
       try {
         if (game.pm) {
           const guildMembers = await channel.guild.members.fetch();
-          const dm = guildMembers.find((m) => m.user.tag === game.dm);
+          console.log(guildMembers.array().length);
+          const dm = guildMembers.find((m) => m.user.tag === ((<RSVP>game.dm).tag || game.dm));
           if (dm && dm.user.dmChannel) {
             const pm = await dm.user.dmChannel.messages.fetch(game.pm);
             if (pm) {
