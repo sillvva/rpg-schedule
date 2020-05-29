fba5fe02dd40a04af945c1e2546793ea3b51cad5
Gavin Mogan <git@gavinmogan.com>
2020-05-29 01:05:28 -0700

    Remove AUTH_URL and INVITE env variables since they can be deterministic

--- src/routes/other.ts
+++ src/routes/other.ts
@@ -6,7 +6,7 @@ export default () => {
   const router = express.Router();
 
   router.use(config.urls.invite.path, (req, res, next) => {
-    res.redirect(process.env.INVITE);
+    res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${process.env.CLIENT_ID}&&scope=bot&permissions=92224`);
   });
 
   router.use(config.urls.donate.path, (req, res, next) => {
